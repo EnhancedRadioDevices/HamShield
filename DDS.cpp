7db28bff0e4b04bb0ae1e200e1654cf4bbbfb86b
Stephen Olesen <slepp@slepp.ca>
2015-07-02 19:30:53 -0600

    Change DDS to using signed integers to try to keep our DC bias averaged out.

--- DDS.cpp
+++ DDS.cpp
@@ -140,9 +140,13 @@ uint8_t DDS::getDutyCycle() {
   uint8_t phAng;
 #endif
   phAng = (accumulator >> ACCUMULATOR_BIT_SHIFT);
-  uint8_t position = pgm_read_byte_near(ddsSineTable + phAng)>>(8-COMPARATOR_BITS);
+  int8_t position = pgm_read_byte_near(ddsSineTable + phAng)>>(8-COMPARATOR_BITS);
   // Apply scaling and return
-  uint16_t scaled = position;
+  int16_t scaled = position;
+  // output = ((duty * amplitude) / 256) + 128
+  // This keeps amplitudes centered around 50% duty
   scaled *= amplitude;
-  return scaled>>8;
+  scaled >>= 8;
+  scaled += 128;
+  return scaled;
 }

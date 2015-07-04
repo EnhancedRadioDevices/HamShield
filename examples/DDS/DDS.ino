7c67a704c3efa872bb99a0566a601b0bb283731e
Stephen Olesen <slepp@slepp.ca>
2015-07-04 02:17:03 -0600

    Use HamShield now, fix refclk rate, increase toggle speed.

--- examples/DDS/DDS.ino
+++ examples/DDS/DDS.ino
@@ -1,22 +1,31 @@
+#define DDS_REFCLK_DEFAULT 9600
 #include <HamShield.h>
 #include <Wire.h>
 
+HamShield radio;
 DDS dds;
 
 void setup() {
+  Wire.begin();
+  radio.initialize();
+  radio.setRfPower(0);
+  radio.setVHF();
+  radio.setFrequency(145060);
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
-  pinMode(11, OUTPUT);
+  pinMode(11, INPUT);
+  radio.setModeTransmit();
   dds.start();
   dds.playWait(600, 3000);
   dds.on();
+  //dds.setAmplitude(31);
 }
 
 void loop() {
   dds.setFrequency(2200);
-  delay(5000);
+  delay(1000);
   dds.setFrequency(1200);
-  delay(5000);
+  delay(1000);
 }
 
 #ifdef DDS_USE_ONLY_TIMER2

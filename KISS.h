a22b814d63e7dc241b1ffa691b7e36dabaff6621
Stephen Olesen <slepp@slepp.ca>
2015-07-13 18:55:07 -0600

    KISS TNC mode for the AX25 stack and example.

---
+++ KISS.h
@@ -0,0 +1,35 @@
+#ifndef _KISS_H_
+#define _KISS_H_
+
+#include <HamShield.h>
+#include "AFSK.h"
+
+#define KISS_FEND 0xC0
+#define KISS_FESC 0xDB
+#define KISS_TFEND 0xDC
+#define KISS_TFESC 0xDD
+
+class KISS {
+public:
+  KISS(Stream *_io, HamShield *h, DDS *d) : io(_io), radio(h), dds(d) {}
+  bool read();
+  void writePacket(AFSK::Packet *);
+  void loop();
+  inline void isr() {
+    static uint8_t tcnt = 0;
+    TIFR1 = _BV(ICF1); // Clear the timer flag
+    dds->clockTick();
+    if(++tcnt == (DDS_REFCLK_DEFAULT/9600)) {
+      PORTD |= _BV(2); // Diagnostic pin (D2)
+      radio->afsk.timer();
+      tcnt = 0;
+    }
+    PORTD &= ~(_BV(2));
+  }
+private:
+  Stream *io;
+  HamShield *radio;
+  DDS *dds;
+};
+
+#endif /* _KISS_H_ */
---
+++ examples/KISS/KISS.ino
@@ -0,0 +1,36 @@
+#define DDS_REFCLK_DEFAULT 19200
+
+#include <HamShield.h>
+#include <Wire.h>
+#include <KISS.h>
+
+HamShield radio;
+DDS dds;
+KISS kiss(&Serial, &radio, &dds);
+
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(11, INPUT);
+  
+  Serial.begin(9600);
+  Wire.begin();
+  radio.initialize();
+  radio.setVHF();
+  radio.setSQOff();
+  radio.setFrequency(145010);
+  I2Cdev::writeWord(A1846S_DEV_ADDR_SENLOW, 0x30, 0x06);
+  I2Cdev::writeWord(A1846S_DEV_ADDR_SENLOW, 0x30, 0x26);
+  I2Cdev::writeWord(A1846S_DEV_ADDR_SENLOW, 0x44, 0b0000011111111111);
+
+  dds.start();
+  radio.afsk.start(&dds);
+}
+
+void loop() {
+  kiss.loop();
+}
+
+ISR(ADC_vect) {
+  kiss.isr();
+}

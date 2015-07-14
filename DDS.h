4f1c863487687877914125415889fef61ec82c50
Stephen Olesen <slepp@slepp.ca>
2015-07-13 18:50:48 -0600

    Default refclk now 9600, remove old table and serial debugging.

--- DDS.h
+++ DDS.h
@@ -46,7 +46,7 @@ typedef uint32_t ddsAccumulator_t;
 // expense of CPU time. It maxes out around 62000 (TBD)
 // May be overridden in the sketch to improve performance
 #ifndef DDS_REFCLK_DEFAULT
-#define DDS_REFCLK_DEFAULT     38400
+#define DDS_REFCLK_DEFAULT     9600
 #endif
 // As each Arduino crystal is a little different, this can be fine tuned to
 // provide more accurate frequencies. Adjustments in the range of hundreds
@@ -61,7 +61,7 @@ typedef uint32_t ddsAccumulator_t;
 #endif
 
 // Output some of the calculations and information about the DDS over serial
-#define DDS_DEBUG_SERIAL
+//#define DDS_DEBUG_SERIAL
 
 // When defined, use the 1024 element sine lookup table. This improves phase
 // accuracy, at the cost of more flash and CPU requirements.
@@ -127,24 +127,6 @@ static const int8_t ddsSineTable[256] PROGMEM = {
   -81, -78, -76, -73, -71, -68, -65, -63, -60, -57, -54, -51, -49,
   -46, -43, -40, -37, -34, -31, -28, -25, -22, -19, -16, -12, -9, -6, -3
   };
-/*static const uint8_t ddsSineTable[256] PROGMEM = {
-  128,131,134,137,140,143,146,149,152,155,158,162,165,167,170,173,
-  176,179,182,185,188,190,193,196,198,201,203,206,208,211,213,215,
-  218,220,222,224,226,228,230,232,234,235,237,238,240,241,243,244,
-  245,246,248,249,250,250,251,252,253,253,254,254,254,255,255,255,
-  255,255,255,255,254,254,254,253,253,252,251,250,250,249,248,246,
-  245,244,243,241,240,238,237,235,234,232,230,228,226,224,222,220,
-  218,215,213,211,208,206,203,201,198,196,193,190,188,185,182,179,
-  176,173,170,167,165,162,158,155,152,149,146,143,140,137,134,131,
-  128,124,121,118,115,112,109,106,103,100,97,93,90,88,85,82,
-  79,76,73,70,67,65,62,59,57,54,52,49,47,44,42,40,
-  37,35,33,31,29,27,25,23,21,20,18,17,15,14,12,11,
-  10,9,7,6,5,5,4,3,2,2,1,1,1,0,0,0,
-  0,0,0,0,1,1,1,2,2,3,4,5,5,6,7,9,
-  10,11,12,14,15,17,18,20,21,23,25,27,29,31,33,35,
-  37,40,42,44,47,49,52,54,57,59,62,65,67,70,73,76,
-  79,82,85,88,90,93,97,100,103,106,109,112,115,118,121,124
-};*/
 #endif /* DDS_TABLE_LARGE */
 
 class DDS {

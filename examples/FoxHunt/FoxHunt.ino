12c9cdf3d9ad0c5bad741d249514eee98d14d694
Nigel Vander Houwen <nigel@nigelvh.com>
2016-06-05 13:01:39 -0700

    Fixed FoxHunt example. Verified working with current library and hardware v09

--- examples/FoxHunt/FoxHunt.ino
+++ examples/FoxHunt/FoxHunt.ino
@@ -6,9 +6,9 @@
 #define RESET_PIN A3
 #define SWITCH_PIN 2
 
-// transmit for 1 minute, every 10 minutes 
-
-#define TRANSMITLENGTH 1
+// In milliseconds
+#define TRANSMITLENGTH 60000
+// In minutes
 #define INTERVAL 10
 #define RANDOMCHANCE 3
 
@@ -27,23 +27,23 @@ void setup() {
   digitalWrite(RESET_PIN, HIGH);
   
   radio.initialize();
-  radio.frequency(145510);
+  radio.setRfPower(0);
+  radio.frequency(438000);
   radio.setModeReceive();
 }
 
 void loop() {
-   waitMinute(INTERVAL + random(0,RANDOMCHANCE));     // wait before transmitting, randomly up to 3 minutes later
-   if(radio.waitForChannel(30000,2000, -90)) {             // wait for a clear channel, abort after 30 seconds, wait 2 seconds of dead air for breakers
-     radio.setModeTransmit();                         // turn on transmit mode
-     tone(1000,11,TRANSMITLENGTH * 60 * 1000);        // play a long solid tone
-     radio.morseOut("1ZZ9ZZ/B FOXHUNT");              // identify the fox hunt transmitter
-     radio.setModeReceive();                          // turn off the transmit mode
-   }
+  if(radio.waitForChannel(30000,2000, -90)) {             // wait for a clear channel, abort after 30 seconds, wait 2 seconds of dead air for breakers
+    radio.setModeTransmit();                         // turn on transmit mode
+    tone(PWM_PIN, 1000, TRANSMITLENGTH);        // play a long solid tone
+    delay(TRANSMITLENGTH);
+    radio.morseOut(" 1ZZ9ZZ/B FOXHUNT");              // identify the fox hunt transmitter
+    radio.setModeReceive();                          // turn off the transmit mode
+  }
+  waitMinute(INTERVAL + random(0,RANDOMCHANCE));     // wait before transmitting, randomly
 }
 
 // a function so we can wait by minutes
-
-void waitMinute(int period) { 
+void waitMinute(unsigned long period) { 
   delay(period * 60 * 1000);
 }
-
